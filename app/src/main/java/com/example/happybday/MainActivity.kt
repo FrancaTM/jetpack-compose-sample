6d982591b760acff862a285ae2b28ef1c0937a90
FrancaTM <francatm@outlook.com>
2023-05-06 18:07:58 -0300

    add image composable

--- app/src/main/java/com/example/happybday/MainActivity.kt
+++ app/src/main/java/com/example/happybday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,6 +60,12 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(painter = image, contentDescription = null)
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = true,
@@ -66,6 +74,6 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBdayTheme {
-        GreetingText("Happy bday to me!!!", from = "From Tulio")
+        GreetingImage("Happy bday to me!!!", from = "From Tulio")
     }
 }
